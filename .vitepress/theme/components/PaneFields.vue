df59e78aa1cfb136357ba3a026ae82c91fd63fc9
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-03 15:46:28 +0530

    trial

--- .vitepress/theme/components/PaneFields.vue
+++ .vitepress/theme/components/PaneFields.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="flex items-center mb-[8px] g-[12px]">
     <span class="flex items-center">
       <div class="text-2xl" :class="icon" />
-      <div class="ml-2 text-[var(--vp-c-text-2)]">
+      <div class="ml-2 text-sm text-[var(--vp-c-text-2)]">
         <slot />
       </div>
     </span>
