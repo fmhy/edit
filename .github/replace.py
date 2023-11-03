b27fdf207eedf4c632c83c86f91be37c0f8c7c65
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-03 21:00:03 +0530

    inline svgs for now, fix replace hopefully

--- .github/replace.py
+++ .github/replace.py
@@ -52,25 +52,25 @@ def remove_backtowiki_and_toc(text):
     return text
 
 def replace_domain_and_page(text):
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/ai',                  '/ai', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/adblock-vpn-privacy', '/adblockvpnguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/android',             '/android-iosguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/video',               '/videopiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/audio',               '/audiopiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/games',               '/gamingpiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/reading',             '/readingpiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/download',            '/downloadpiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/torrent',             '/torrentpiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/edu',                 '/edupiracyguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/tools-misc',          '/toolsguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/linux',               '/linuxguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/non-eng',             '/non-english', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/misc',                '/miscguide', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/storage',             '/storage', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/dev-tools',           '/devtools', text)
-    text = re.sub('www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/img-tools',           '/img-tools', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/ai',                  '/ai', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/adblock-vpn-privacy', '/adblockvpnguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/android',             '/android-iosguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/video',               '/videopiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/audio',               '/audiopiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/games',               '/gamingpiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/reading',             '/readingpiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/download',            '/downloadpiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/torrent',             '/torrentpiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/edu',                 '/edupiracyguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/tools-misc',          '/toolsguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/linux',               '/linuxguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/non-eng',             '/non-english', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/misc',                '/miscguide', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/storage',             '/storage', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/dev-tools',           '/devtools', text)
+    text = re.sub('https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/img-tools',           '/img-tools', text)  
 
-    text = re.sub('github.com/nbats/FMHYedit/blob/main/base64.md#',              '/base64/#', text)
+    text = re.sub('https://github.com/nbats/FMHYedit/blob/main/base64.md#',              '/base64/#', text)
 
     return text
 
--- .github/script.sh
+++ .github/script.sh
@@ -1,6 +1,6 @@
 python .github/add-headers.py
+python .github/replace.py
 
 for file in *; do
     [[ -f "$file" ]] && mv "$file" "${file,,}" 2>/dev/null
 done
-python .github/replace.py
