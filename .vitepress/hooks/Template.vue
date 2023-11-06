cc91903e2b3debc5238f070c749edf495f24afbc
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-06 21:33:53 +0530

    remove svg if that fixes it?

--- .vitepress/hooks/Template.vue
+++ .vitepress/hooks/Template.vue
@@ -3,17 +3,10 @@ defineProps<{ title: string; description?: string; dir?: string }>();
 </script>
 
 <template>
-  <div
-    tw="w-full h-full bg-black flex flex-col"
-    style="background-image: url(https://files.catbox.moe/1f84dy.png)">
+  <div tw="w-full h-full bg-black flex flex-col" style="background-image: url(https://files.catbox.moe/1f84dy.png)">
     <div tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between">
       <div tw="w-full flex justify-between items-center text-5xl font-medium">
         <div tw="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 256 256">
-            <path
-              fill="#f3f4f6"
-              d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128Z" />
-          </svg>
           <div tw="text-zinc-100 ml-2 mt-1 font-semibold">freemediaheckyeah</div>
         </div>
         <div tw="flex items-center text-zinc-300">
