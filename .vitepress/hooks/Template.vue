9f98f76e57939ba1793beb9c8e5208e1c8ab3e9a
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-06 21:53:07 +0530

    try to fix div soup

--- .vitepress/hooks/Template.vue
+++ .vitepress/hooks/Template.vue
@@ -3,18 +3,14 @@ defineProps<{ title: string; description?: string }>();
 </script>
 
 <template>
-  <div
-    tw="w-full h-full bg-black flex flex-col"
-    style="background-image: url(https://files.catbox.moe/1f84dy.png)">
+  <div tw="w-full h-full bg-black flex flex-col" style="background-image: url(https://files.catbox.moe/1f84dy.png)">
     <div tw="p-10 w-full min-h-0 grow flex flex-col items-center justify-between">
       <div tw="w-full flex justify-between items-center text-5xl font-medium">
-        <div tw="flex items-center">
-          <div tw="text-zinc-100 ml-2 mt-1 font-semibold">freemediaheckyeah</div>
-        </div>
+        <div tw="text-zinc-100 ml-2 mt-1 font-semibold">freemediaheckyeah</div>
       </div>
       <div tw="w-full pr-56 flex flex-col items-start justify-end">
         <div tw="text-6xl font-bold text-stone-200" v-html="title" />
-        <div v-if="description" tw="mt-2 text-4xl text-neutral-400" v-html="description" />
+        <div tw="mt-2 text-4xl text-neutral-400" v-html="description" />
       </div>
     </div>
     <div tw="shrink-0 h-4 w-full flex" style="background-color: #7bc5e4" />
