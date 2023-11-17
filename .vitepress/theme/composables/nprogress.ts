3c525af917bcc4c9b12a0072057dbdab66d25fe2
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-11-17 13:26:59 +0530

    Show progress on route change

---
+++ .vitepress/theme/composables/nprogress.ts
@@ -0,0 +1,23 @@
+import nprogress, { type NProgress } from "nprogress";
+import type { EnhanceAppContext } from "vitepress";
+
+export function loadProgress(router: EnhanceAppContext["router"]): NProgress {
+  if (typeof window === "undefined") return;
+
+  setTimeout(() => {
+    nprogress.configure({ showSpinner: false });
+
+    const cacheBeforeRouteChange = router.onBeforeRouteChange;
+    const cacheAfterRouteChange = router.onAfterRouteChanged;
+    router.onBeforeRouteChange = (to) => {
+      nprogress.start();
+      cacheBeforeRouteChange?.(to);
+    };
+    router.onAfterRouteChanged = (to) => {
+      nprogress.done();
+      cacheAfterRouteChange?.(to);
+    };
+  });
+
+  return nprogress;
+}
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,14 @@
 import { type Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import Layout from "./Layout.vue";
+import { loadProgress } from "./composables/nprogress";
 import "./style.css";
 import "uno.css";
 
 export default {
   extends: DefaultTheme,
   Layout,
-  enhanceApp({ app, router, siteData }) {},
+  enhanceApp({ router }) {
+    loadProgress(router);
+  },
 } satisfies Theme;
