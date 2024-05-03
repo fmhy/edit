cd213aa100c9194c373596d45e00e33b9d210dea
maropboia <164220066+maropboia@users.noreply.github.com>
2024-05-03 11:38:35 +0600

    comment

--- .vitepress/hooks/index.ts
+++ .vitepress/hooks/index.ts
@@ -1,8 +1,16 @@
 /**
- * Barrel generated using @taskylizard/tasker.
+ * This file serves as a barrel for exporting various modules related to metadata generation.
+ * It utilizes the `@taskylizard/tasker` tool for barrel generation.
+ *
+ * The following modules are re-exported from their respective files:
+ * - meta: Contains functions for generating metadata relevant to the entire website.
+ * - opengraph: Contains functions for generating Open Graph metadata for individual pages.
+ * - rss: Contains functions for generating RSS feed metadata.
+ * - satoriConfig: Contains configuration settings for the Satori metadata generation tool.
  */
 
 export * from './meta'
 export * from './opengraph'
 export * from './rss'
 export * from './satoriConfig'
+
