a69a834899f05f437efcd2bd91e729225e6c8662
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-07-10 03:47:04 +0000

    feat(api): ratelimiting
    because some group of idiots like me so much that they spam my endpoint

--- .vitepress/routes/index.post.ts
+++ .vitepress/routes/index.post.ts
@@ -7,6 +7,14 @@ export default defineEventHandler(async (event) => {
     FeedbackSchema.parseAsync
   )
   const env = useRuntimeConfig(event)
+  const { pathname } = new URL(event.node.req.url)
+
+  const { success } = await env.MY_RATE_LIMITER.limit({ key: pathname })
+  if (!success) {
+    return new Response(`429 Failure – rate limit exceeded for ${pathname}`, {
+      status: 429
+    })
+  }
 
   let description = `${message}\n\n`
   if (page) description += `**Page:** \`${page}\``
