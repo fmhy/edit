9fd6157ec9f575690c165344b3b10aff7e9be985
rhld16 <24809571+rhld16@users.noreply.github.com>
2025-01-19 15:56:09 +0000

    fix(md): fix some markdown rendering issues (#2862)
    * fix(md): correctly append feedback on linked headings
    
    * fix(md): make sure to use vitepress' default link renderer

--- docs/.vitepress/markdown/headers.ts
+++ docs/.vitepress/markdown/headers.ts
@@ -40,7 +40,7 @@ export const headersPlugin = (md: MarkdownRenderer) => {
     //
     // The token after `heading_open` contains the link as a child token.
     const children = tokens[idx + 1].children || []
-    const linkOpenToken = children.find((c) => c.type === 'link_open')
+    const linkOpenToken = children.findLast((c) => c.type === 'link_open')
     if (!linkOpenToken) return result
 
     const heading = tokens[idxClose - 1]
@@ -53,8 +53,10 @@ export const headersPlugin = (md: MarkdownRenderer) => {
     return result
   }
 
+  let defaultRender = md.renderer.rules.link_open;
+
   md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
-    const result = self.renderToken(tokens, idx, options)
+    const result = defaultRender(tokens, idx, options, env, self);
 
     const meta = tokens[idx].meta
     if (!meta || !meta.feedback) return result
