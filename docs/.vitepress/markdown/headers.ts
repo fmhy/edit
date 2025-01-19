bfe76126d90855dc67f488a41b3d8d1f4e11e624
rhld16 <rhld016@protonmail.com>
2025-01-19 15:54:23 +0000

    fix(md): make sure to use vitepress' default link renderer

--- docs/.vitepress/markdown/headers.ts
+++ docs/.vitepress/markdown/headers.ts
@@ -53,8 +53,10 @@ export const headersPlugin = (md: MarkdownRenderer) => {
     return result
   }
 
+  let defaultRender = md.renderer.rules.link_open;
+
   md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
-    const result = self.renderToken(tokens, idx, options)
+    const result = defaultRender(tokens, idx, options, env, self);
 
     const meta = tokens[idx].meta
     if (!meta || !meta.feedback) return result
