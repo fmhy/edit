01dfd2ec26a8ad504baed5157b283599ab60e524
Eason Li <lyu63651@gmail.com>
2026-01-05 09:12:06 +0800

    Add indexes toggle and disable starred

---
+++ docs/.vitepress/theme/components/ToggleIndexes.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Switch from './Switch.vue'
+
+const toggleIndexes = () =>
+  document.documentElement.classList.toggle('indexes-only')
+</script>
+
+<template>
+  <Switch @click="toggleIndexes()" />
+</template>
+
+<style>
+.indexes-only li:not(.index) {
+  display: none;
+}
+</style>
--- docs/.vitepress/theme/components/ToggleStarred.vue
+++ docs/.vitepress/theme/components/ToggleStarred.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import Switch from './Switch.vue'
 
-const toggleStarred = () =>
+const isDisabled = ref(false)
+
+const syncDisabled = () => {
+  isDisabled.value = document.documentElement.classList.contains('indexes-only')
+}
+
+let observer: MutationObserver | undefined
+
+onMounted(() =>
+  (observer = new MutationObserver(syncDisabled)).observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['class']
+  })
+)
+
+onMounted(syncDisabled)
+
+onBeforeUnmount(() => observer?.disconnect())
+
+const toggleStarred = () => {
+  if (isDisabled.value) return
   document.documentElement.classList.toggle('starred-only')
+}
 </script>
 
 <template>
-  <Switch @click="toggleStarred()" />
+  <Switch :class="{ disabled: isDisabled }" @click="toggleStarred()" />
 </template>
 
 <style>
