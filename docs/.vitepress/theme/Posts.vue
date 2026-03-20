5aa48a6fbe38ac89116f1149f68b9a9fe5ef08c0
litekin <finder-5000@proton.me>
2026-03-20 05:49:39 +0000

    edit posts page

--- docs/.vitepress/theme/Posts.vue
+++ docs/.vitepress/theme/Posts.vue
@@ -15,11 +15,8 @@ const formatDate = (raw: string): string => {
   <div>
     <section>
       <h1 class="flex items-center gap-2">Posts</h1>
-      <p>Everything from Monthly Updates to fmhy updates.</p>
-
-      We also have a
-      <a href="/feed.rss" target="_blank" title="RSS feed">
-        <div class="i-carbon-rss vertical-top" />
+      <p>Monthly updates, announcements, and more.</p> We also have an <a href="/feed.rss" target="_blank" title="RSS feed">
+        <div class="i-carbon-rss vertical-top" style="width: 16px; height: 24px;" />
         RSS feed.
       </a>
     </section>
